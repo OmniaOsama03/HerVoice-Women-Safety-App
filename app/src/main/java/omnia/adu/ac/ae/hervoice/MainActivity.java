b3b1ed69d52a03e1367aa5c693492d5f1387bc35
OmniaOsama03 <Omniaosama432@gmail.com>
2025-05-23 19:15:17 +0400

    Login v1.0
    Database Manager, Session Manager, Login,

--- app/src/main/java/omnia/adu/ac/ae/hervoice/MainActivity.java
+++ app/src/main/java/omnia/adu/ac/ae/hervoice/MainActivity.java
@@ -2,76 +2,152 @@ package omnia.adu.ac.ae.hervoice;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.*;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import omnia.adu.ac.ae.hervoice.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailInput, passwordInput;
+    TextView emailValidation, passwordValidation;
+    Button loginButton, registerButton;
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    boolean isEmailValid = false;
+    boolean isPasswordValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //Link views
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        emailValidation = findViewById(R.id.emailValidation);
+        passwordValidation = findViewById(R.id.passwordValidation);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
 
-        setSupportActionBar(binding.toolbar);
+        //Text watchers
+        emailInput.addTextChangedListener(new EmailWatcher());
+        passwordInput.addTextChangedListener(new PasswordWatcher());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        //Login button click
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                //Ensuring neither email nor password have '
+                if (email.contains("'") || password.contains("'"))
+                {
+                    Toast.makeText(MainActivity.this, "Invalid character: ' not allowed", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                DatabaseManager db = DatabaseManager.getInstance(MainActivity.this);
+                boolean success = db.loginUser(email, password);
+
+                if (success)
+                {
+                    Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(MainActivity.this, HomePageActivity.class);
+                    startActivity(i);
+
+                    finish();
+
+                } else
+                {
+                    passwordValidation.setText("Incorrect email or password :(");
+                    passwordValidation.setTextColor(getResources().getColor(R.color.deep_red));
+                }
             }
         });
+
+        // Register button click
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    // Email validation watcher
+    private class EmailWatcher implements TextWatcher
+    {
+        @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            String email = s.toString().trim();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            if (!email.contains("@") || email.contains("'"))
+            {
+                emailValidation.setText("Invalid email.");
+                isEmailValid = false;
+            } else {
+                emailValidation.setText("");
+                isEmailValid = true;
+            }
+
+            loginButton.setEnabled(isEmailValid && isPasswordValid);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    // Password validation watcher
+    private class PasswordWatcher implements TextWatcher {
+        @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            String password = s.toString();
+
+            if (!isPasswordValid(password))
+            {
+                passwordValidation.setText("Invalid password.");
+                isPasswordValid = false;
+            } else {
+                passwordValidation.setText("");
+                isPasswordValid = true;
+            }
+
+            loginButton.setEnabled(isEmailValid && isPasswordValid);
+        }
+    }
+
+    //Password rules
+    private boolean isPasswordValid(String password) {
+        if (password.length() < 8)
+            return false;
+
+        if (!password.matches("[A-Z0-9*%#&a-z]+"))
+            return false;
+
+        if (!password.matches(".*\\d.*\\d.*"))
+            return false;
+
+        if (!password.matches(".*[A-Z].*"))
+            return false;
+
+        if (!password.matches(".*[a-z].*"))
+            return false;
+
+        return true;
     }
 }
